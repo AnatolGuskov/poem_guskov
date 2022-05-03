a2de3c78a6cbdca362999385b28667fb5f79e601
AnatolGuskov <kursor@i.ua>
2022-05-03 13:32:58 +0300

    Admin

--- poems/admin.py
+++ poems/admin.py
@@ -4,22 +4,28 @@ from django.contrib import admin
 from .models import Language, Author, Genre, Poem, Book
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'middl_name', 'date_of_birth', )
+    list_display = ('last_name', 'first_name', 'middl_name', 'date_of_birth', 'id' )
 admin.site.register(Author, AuthorAdmin)
 
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author')
+    list_display = ('title', 'id', 'author', )
 admin.site.register(Book, BookAdmin)
 
 
 class PoemAdmin(admin.ModelAdmin):
-    list_display = ('date', 'tytle', 'author')
+    list_display = ('tytle', 'id', 'date', 'author',)
 admin.site.register(Poem, PoemAdmin)
 
 
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+admin.site.register(Genre, GenreAdmin)
+
+
+
 admin.site.register(Language)
-admin.site.register(Genre)
+
 # admin.site.register(Author)
 
 
