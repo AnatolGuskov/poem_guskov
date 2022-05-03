8f514582a96328ff7d97b254b3da7887c4d2ea9a
AnatolGuskov <kursor@i.ua>
2022-05-03 13:15:44 +0300

    Admin

--- poems/admin.py
+++ poems/admin.py
@@ -5,16 +5,23 @@ from .models import Language, Author, Genre, Poem, Book
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'middl_name', 'date_of_birth', )
-
 admin.site.register(Author, AuthorAdmin)
 
+
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
+    list_display = ('title', 'author')
+admin.site.register(Book, BookAdmin)
+
+
+class PoemAdmin(admin.ModelAdmin):
+    list_display = ('date', 'title', 'author')
+admin.site.register(Poem, PoemAdmin)
+
 
 admin.site.register(Language)
 admin.site.register(Genre)
 # admin.site.register(Author)
-admin.site.register(Poem)
-admin.site.register(Book)
+
+
 
 
